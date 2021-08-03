3351a48bdf4ef45eda70d32bbed12dff040b7ba9
andytudhope <tuddy0525@gmail.com>
2021-08-03 14:43:23 +0200

    o knowledge, pleasure, inexhaustible

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $.get("https://api.opensea.io/api/v1/assets?order_direction=asc&offset=0&asset_contract_address=0xeB3fC95B74C79C2c3469188A72df2c2399D752AB",function(allIds){
     window.chapter1url = allIds.assets[0].external_link;
     window.chapter2url = allIds.assets[1].external_link;
+    window.chapter3url = allIds.assets[2].external_link;
     
     $.get(window.chapter1url.toString(),function(chap1){
         $("#one").html(chap1.toString());
@@ -10,5 +11,10 @@ $.get("https://api.opensea.io/api/v1/assets?order_direction=asc&offset=0&asset_c
     $.get(window.chapter2url.toString(),function(chap2){
         $("#two").html(chap2.toString());
         console.log("the guardians have ensured you are seeing chapter 2 content from " + window.chapter2url);
+    });
+
+    $.get(window.chapter3url.toString(),function(chap3){
+        $("#two").html(chap3.toString());
+        console.log("the guardians have ensured you are seeing chapter 3 content from " + window.chapter3url);
     });
 });
