da586c16fa94c3af30b529272de0676a8461c64a
andytudhope <13001517+andytudhope@users.noreply.github.com>
2023-08-30 11:59:48 -0400

    brotherly loving

--- tasks/operations/mint-token.ts
+++ tasks/operations/mint-token.ts
@@ -3,7 +3,7 @@ import { ContractTransaction } from "ethers";
 import { Guardians } from "../../typechain";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { TASK_MINT } from "../task-names";
-// npx hardhat mint-token --network mainnet --metadata-uri ar://YpiqlKAGEwIqtiwd76o7skWoe5Ncmn7GCHeftHdxSiI
+// npx hardhat mint-token --network mainnet --metadata-uri ar://S-0sEx3OBr9V3jKbXC0XgSoIbp7kOEqiH637eYh3dOM
 task(TASK_MINT, "Mints a token with token metadata uri")
   .addParam("metadataUri", "The token URI", null, types.string)
   .setAction(async ({ metadataUri }, hre) => {
